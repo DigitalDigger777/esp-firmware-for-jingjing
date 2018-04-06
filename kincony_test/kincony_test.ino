8503f00126784503665ae60d2c00d130a741a267
korman <korman.yuri@gmail.com>
2018-04-06 13:42:01 +0300

    add kincony test

--- kincony_test/kincony_test.ino
+++ kincony_test/kincony_test.ino
@@ -15,7 +15,17 @@ bool shouldSaveConfig = false;
 //bool newDevice = true;
 int delayInterval = 3000;
 
-int pin = 12; //pin for Node MCU
+int pin0 = 16; //pin for Node MCU
+int pin1 = 5;
+int pin2 = 4;
+int pin3 = 0;
+int pin4 = 2;
+int pin5 = 14;
+int pin6 = 12;
+int pin7 = 13;
+int pin8 = 15;
+int pin9 = 3;
+int pin10 = 1;
 
 void setup() {
   //init serial
@@ -26,7 +36,19 @@ void setup() {
   
   wifiManager.autoConnect();
 
-  pinMode(pin, OUTPUT);
+  pinMode(pin0, OUTPUT);
+  pinMode(pin1, OUTPUT);
+  pinMode(pin2, OUTPUT);
+  pinMode(pin3, OUTPUT);
+  pinMode(pin4, OUTPUT);
+  pinMode(pin5, OUTPUT);
+  pinMode(pin6, OUTPUT);
+  pinMode(pin7, OUTPUT);
+  pinMode(pin8, OUTPUT);
+  pinMode(pin9, OUTPUT);
+  pinMode(pin10, OUTPUT);
+
+  pinMode(LED_BUILTIN, OUTPUT);
   
 }
 
@@ -35,9 +57,362 @@ void loop() {
   
   if((WiFiMulti.run() == WL_CONNECTED)) {
     Serial.println("...start...");
-    digitalWrite(pin, LOW);
-    delay(delayInterval);
-    digitalWrite(pin, HIGH);
+    
+    digitalWrite(pin0, HIGH);
+    delay(1000);
+    digitalWrite(pin0, LOW);
+
+    delay(10000);
+    
+    digitalWrite(pin1, HIGH);
+    delay(1000);
+    digitalWrite(pin1, LOW);
+    delay(1000);
+    
+    digitalWrite(pin1, HIGH);
+    delay(1000);
+    digitalWrite(pin1, LOW);
+    delay(1000);
+    
+    delay(10000);
+    
+    digitalWrite(pin2, HIGH);
+    delay(1000);
+    digitalWrite(pin2, LOW);
+    delay(1000);
+    
+    digitalWrite(pin2, HIGH);
+    delay(1000);
+    digitalWrite(pin2, LOW);
+    delay(1000);
+    
+    digitalWrite(pin2, HIGH);
+    delay(1000);
+    digitalWrite(pin2, LOW);
+    delay(1000);
+    
+    delay(10000);
+    
+    digitalWrite(pin3, HIGH);
+    delay(1000);
+    digitalWrite(pin3, LOW);
+    delay(1000);
+
+    digitalWrite(pin3, HIGH);
+    delay(1000);
+    digitalWrite(pin3, LOW);
+    delay(1000);
+    
+    digitalWrite(pin3, HIGH);
+    delay(1000);
+    digitalWrite(pin3, LOW);
+    delay(1000);
+    
+    digitalWrite(pin3, HIGH);
+    delay(1000);
+    digitalWrite(pin3, LOW);
+    delay(1000);
+    
+    delay(10000);
+
+    digitalWrite(pin4, HIGH);
+    delay(1000);
+    digitalWrite(pin4, LOW);
+    delay(1000);
+
+    digitalWrite(pin4, HIGH);
+    delay(1000);
+    digitalWrite(pin4, LOW);
+    delay(1000);
+
+    digitalWrite(pin4, HIGH);
+    delay(1000);
+    digitalWrite(pin4, LOW);
+    delay(1000);
+    
+    digitalWrite(pin4, HIGH);
+    delay(1000);
+    digitalWrite(pin4, LOW);
+    delay(1000);
+
+    digitalWrite(pin4, HIGH);
+    delay(1000);
+    digitalWrite(pin4, LOW);
+    delay(1000);
+    
+    delay(10000);
+    
+    digitalWrite(pin5, HIGH);
+    delay(1000);
+    digitalWrite(pin5, LOW);
+    delay(1000);
+    
+    digitalWrite(pin5, HIGH);
+    delay(1000);
+    digitalWrite(pin5, LOW);
+    delay(1000);
+
+    digitalWrite(pin5, HIGH);
+    delay(1000);
+    digitalWrite(pin5, LOW);
+    delay(1000);
+    
+    digitalWrite(pin5, HIGH);
+    delay(1000);
+    digitalWrite(pin5, LOW);
+    delay(1000);
+
+    digitalWrite(pin5, HIGH);
+    delay(1000);
+    digitalWrite(pin5, LOW);
+    delay(1000);
+
+    digitalWrite(pin5, HIGH);
+    delay(1000);
+    digitalWrite(pin5, LOW);
+    delay(1000);
+
+    digitalWrite(pin5, HIGH);
+    delay(1000);
+    digitalWrite(pin5, LOW);
+    delay(1000);
+    
+    delay(10000);
+
+    digitalWrite(pin6, HIGH);
+    delay(1000);
+    digitalWrite(pin6, LOW);
+    delay(1000);
+
+    digitalWrite(pin6, HIGH);
+    delay(1000);
+    digitalWrite(pin6, LOW);
+    delay(1000);
+
+    digitalWrite(pin6, HIGH);
+    delay(1000);
+    digitalWrite(pin6, LOW);
+    delay(1000);
+
+    digitalWrite(pin6, HIGH);
+    delay(1000);
+    digitalWrite(pin6, LOW);
+    delay(1000);
+    
+    digitalWrite(pin6, HIGH);
+    delay(1000);
+    digitalWrite(pin6, LOW);
+    delay(1000);
+
+    digitalWrite(pin6, HIGH);
+    delay(1000);
+    digitalWrite(pin6, LOW);
+    delay(1000);
+
+    digitalWrite(pin6, HIGH);
+    delay(1000);
+    digitalWrite(pin6, LOW);
+    delay(1000);
+    
+    delay(10000);
+
+    digitalWrite(pin7, HIGH);
+    delay(1000);
+    digitalWrite(pin7, LOW);
+    delay(1000);
+
+    digitalWrite(pin7, HIGH);
+    delay(1000);
+    digitalWrite(pin7, LOW);
+    delay(1000);
+
+    digitalWrite(pin7, HIGH);
+    delay(1000);
+    digitalWrite(pin7, LOW);
+    delay(1000);
+
+    digitalWrite(pin7, HIGH);
+    delay(1000);
+    digitalWrite(pin7, LOW);
+    delay(1000);
+
+    digitalWrite(pin7, HIGH);
+    delay(1000);
+    digitalWrite(pin7, LOW);
+    delay(1000);
+
+    digitalWrite(pin7, HIGH);
+    delay(1000);
+    digitalWrite(pin7, LOW);
+    delay(1000);
+
+    digitalWrite(pin7, HIGH);
+    delay(1000);
+    digitalWrite(pin7, LOW);
+    delay(1000);
+
+    digitalWrite(pin7, HIGH);
+    delay(1000);
+    digitalWrite(pin7, LOW);
+    delay(1000);
+    
+    delay(10000);
+
+    digitalWrite(pin8, HIGH);
+    delay(1000);
+    digitalWrite(pin8, LOW);
+    delay(1000);
+
+    digitalWrite(pin8, HIGH);
+    delay(1000);
+    digitalWrite(pin8, LOW);
+    delay(1000);
+
+    digitalWrite(pin8, HIGH);
+    delay(1000);
+    digitalWrite(pin8, LOW);
+
+    digitalWrite(pin8, HIGH);
+    delay(1000);
+    digitalWrite(pin8, LOW);
+    delay(1000);
+
+    digitalWrite(pin8, HIGH);
+    delay(1000);
+    digitalWrite(pin8, LOW);
+
+    digitalWrite(pin8, HIGH);
+    delay(1000);
+    digitalWrite(pin8, LOW);
+
+    digitalWrite(pin8, HIGH);
+    delay(1000);
+    digitalWrite(pin8, LOW);
+    delay(1000);
+
+    digitalWrite(pin8, HIGH);
+    delay(1000);
+    digitalWrite(pin8, LOW);
+    delay(1000);
+
+    digitalWrite(pin8, HIGH);
+    delay(1000);
+    digitalWrite(pin8, LOW);
+    delay(1000);
+
+    delay(10000);
+
+    digitalWrite(pin9, HIGH);
+    delay(1000);
+    digitalWrite(pin9, LOW);    
+    delay(1000);
+
+    digitalWrite(pin9, HIGH);
+    delay(1000);
+    digitalWrite(pin9, LOW);    
+    delay(1000);
+    
+    digitalWrite(pin9, HIGH);
+    delay(1000);
+    digitalWrite(pin9, LOW);    
+    delay(1000);
+
+    digitalWrite(pin9, HIGH);
+    delay(1000);
+    digitalWrite(pin9, LOW);    
+    delay(1000);
+    
+    digitalWrite(pin9, HIGH);
+    delay(1000);
+    digitalWrite(pin9, LOW);    
+    delay(1000);
+    
+    digitalWrite(pin9, HIGH);
+    delay(1000);
+    digitalWrite(pin9, LOW);    
+    delay(1000);
+
+    digitalWrite(pin9, HIGH);
+    delay(1000);
+    digitalWrite(pin9, LOW);    
+    delay(1000);
+
+    digitalWrite(pin9, HIGH);
+    delay(1000);
+    digitalWrite(pin9, LOW);    
+    delay(1000);
+    
+    digitalWrite(pin9, HIGH);
+    delay(1000);
+    digitalWrite(pin9, LOW);    
+    delay(1000);
+
+    digitalWrite(pin9, HIGH);
+    delay(1000);
+    digitalWrite(pin9, LOW);    
+    delay(1000);
+
+    digitalWrite(pin9, HIGH);
+    delay(1000);
+    digitalWrite(pin9, LOW);    
+    delay(1000);
+    
+    delay(10000);
+
+    digitalWrite(pin10, HIGH);
+    delay(1000);
+    digitalWrite(pin10, LOW);
+    delay(1000);
+
+    digitalWrite(pin10, HIGH);
+    delay(1000);
+    digitalWrite(pin10, LOW);
+    delay(1000);
+
+    digitalWrite(pin10, HIGH);
+    delay(1000);
+    digitalWrite(pin10, LOW);
+    delay(1000);
+
+    digitalWrite(pin10, HIGH);
+    delay(1000);
+    digitalWrite(pin10, LOW);
+    delay(1000);
+
+    digitalWrite(pin10, HIGH);
+    delay(1000);
+    digitalWrite(pin10, LOW);
+    delay(1000);
+
+    digitalWrite(pin10, HIGH);
+    delay(1000);
+    digitalWrite(pin10, LOW);
+    delay(1000);
+
+    digitalWrite(pin10, HIGH);
+    delay(1000);
+    digitalWrite(pin10, LOW);
+    delay(1000);
+
+    digitalWrite(pin10, HIGH);
+    delay(1000);
+    digitalWrite(pin10, LOW);
+    delay(1000);
+
+    digitalWrite(pin10, HIGH);
+    delay(1000);
+    digitalWrite(pin10, LOW);
+    delay(1000);
+
+    digitalWrite(pin10, HIGH);
+    delay(1000);
+    digitalWrite(pin10, LOW);
+    delay(1000);
+
+    digitalWrite(pin10, HIGH);
+    delay(1000);
+    digitalWrite(pin10, LOW);
+    delay(1000);
   }
-  delay(delayInterval);
+
 }
